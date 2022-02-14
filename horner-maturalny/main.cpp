8ae0905ddcfefaa2730c81d34e0848110aaba54c
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-14 21:04:58 +0100

    Initial Commit

---
+++ horner-maturalny/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  horner-maturalny
+//
+//  Created by Kuba Wtorkiewicz on 14/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
